6e870a85caad49891d260e4e1db5f86843e8eddb
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-03-21 21:39:08 +0000

    .

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: integer to be computed
+ * Return: absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	return (n);
+}
