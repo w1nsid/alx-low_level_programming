792a57d41ffd02e2611e70583d95b4b45811e1ed
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-03-22 00:34:17 +0000

    .

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 
-/**
- * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
- * Return: 0
- */
+#define MAX_DIGITS 200
 
 int main(void)
 {
-	int i;
-	unsigned long a, b, c;
+	int i, j, k, carry;
+	int fib[MAX_DIGITS] = {1};
+	int prev[MAX_DIGITS] = {1};
+	int prev2[MAX_DIGITS] = {0};
 
-	a = 1;
-	b = 2;
-	printf("%lu, %lu, ", a, b);
-	for (i = 0; i < 96; i++)
+	printf("1");
+	for (i = 2; i <= 98; i++)
 	{
-		c = a + b;
-		a = b;
-		b = c;
-		printf("%lu", c);
-
-		if (i != 95)
+		carry = 0;
+		for (j = 0; j < MAX_DIGITS; j++)
 		{
-			printf(", ");
+			fib[j] = prev[j] + prev2[j] + carry;
+			carry = fib[j] / 10;
+			fib[j] %= 10;
+		}
+		printf(", ");
+		for (k = MAX_DIGITS - 1; k >= 0; k--)
+		{
+			if (fib[k] != 0)
+				break;
+		}
+		for (; k >= 0; k--)
+		{
+			printf("%d", fib[k]);
+		}
+		for (j = 0; j < MAX_DIGITS; j++)
+		{
+			prev2[j] = prev[j];
+			prev[j] = fib[j];
 		}
 	}
-
 	printf("\n");
-	return (0);
+	return 0;
 }
