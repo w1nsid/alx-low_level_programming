b433515e8f8cf23e695962f5d1aba33b388ce50e
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-04-06 00:34:05 +0000

    fix indent

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,7 +11,7 @@ int _sqrt(int n, int i);
  */
 int _sqrt_recursion(int n)
 {
-  return (_sqrt(n, 1));
+	return (_sqrt(n, 1));
 }
 
 /**
@@ -23,12 +23,12 @@ int _sqrt_recursion(int n)
  */
 int _sqrt(int n, int i)
 {
-  int sqrt = i * i;
+	int sqrt = i * i;
 
-  if (sqrt > n)
-    return (-1);
+	if (sqrt > n)
+		return (-1);
 
-  if (sqrt == n)
-    return (i);
-  return (_sqrt(n, i + 1));
+	if (sqrt == n)
+		return (i);
+	return (_sqrt(n, i + 1));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,7 +11,7 @@ int check_prime(int n, int i);
  */
 int is_prime_number(int n)
 {
-  return (check_prime(n, 1));
+	return (check_prime(n, 1));
 }
 
 /**
@@ -23,14 +23,14 @@ int is_prime_number(int n)
  */
 int check_prime(int n, int i)
 {
-  if (n <= 1)
-    return (0);
+	if (n <= 1)
+		return (0);
 
-  if (n % i == 0 && i > 1)
-    return (0);
+	if (n % i == 0 && i > 1)
+		return (0);
 
-  if ((n / i) < i)
-    return (1);
+	if ((n / i) < i)
+		return (1);
 
-  return (check_prime(n, i + 1));
+	return (check_prime(n, i + 1));
 }
