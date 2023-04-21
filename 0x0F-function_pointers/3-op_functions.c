e6a6635cf1fa7d4916d1fe22c1ff356b01b70371
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-04-21 00:22:03 +0000

    fix

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -11,7 +11,7 @@
  */
 int op_add(int a, int b)
 {
-  return (a + b);
+	return (a + b);
 }
 
 /**
@@ -23,7 +23,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-  return (a - b);
+	return (a - b);
 }
 
 /**
@@ -35,7 +35,7 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-  return (a * b);
+	return (a * b);
 }
 
 /**
@@ -47,13 +47,13 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-  if (b == 0)
-  {
-    printf("Error\n");
-    exit(100);
-  }
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-  return (a / b);
+	return (a / b);
 }
 
 /**
@@ -65,11 +65,11 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-  if (b == 0)
-  {
-    printf("Error\n");
-    exit(100);
-  }
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-  return (a % b);
+	return (a % b);
 }
