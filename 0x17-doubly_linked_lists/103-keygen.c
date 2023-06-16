ff96616731256331565a8423c3b1ee00a3de6c2e
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-06-16 01:14:58 +0100

    .

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -12,8 +12,9 @@ char *generate_key(const char *username)
 {
 	unsigned int i, value;
 	size_t len, sum;
-	char *charset = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char *s = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char *key = malloc(7 * sizeof(char));
+
 	if (key == NULL)
 	{
 		printf("Memory allocation failed\n");
@@ -21,31 +22,31 @@ char *generate_key(const char *username)
 	}
 
 	len = strlen(username);
-	key[0] = charset[(len ^ 59) & 63];
+	key[0] = s[(len ^ 59) & 63];
 
 	for (i = 0, sum = 0; i < len; i++)
 		sum += username[i];
-	key[1] = charset[(sum ^ 79) & 63];
+	key[1] = s[(sum ^ 79) & 63];
 
 	for (i = 0, value = 1; i < len; i++)
 		value *= username[i];
-	key[2] = charset[(value ^ 85) & 63];
+	key[2] = s[(value ^ 85) & 63];
 
 	for (value = username[0], i = 0; i < len; i++)
 		if ((char)value <= username[i])
 			value = username[i];
 	srand(value ^ 14);
-	key[3] = charset[rand() & 63];
+	key[3] = s[rand() & 63];
 
 	for (value = 0, i = 0; i < len; i++)
 		value += username[i] * username[i];
-	key[4] = charset[(value ^ 239) & 63];
+	key[4] = s[(value ^ 239) & 63];
 
 	for (value = 0, i = 0; (char)i < username[0]; i++)
 		value = rand();
-	key[5] = charset[(value ^ 229) & 63];
+	key[5] = s[(value ^ 229) & 63];
 
-	return key;
+	return (key);
 }
 
 /**
@@ -62,12 +63,12 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Correct usage: ./keygen5 username\n");
-		return 1;
+		return (1);
 	}
 
 	key = generate_key(argv[1]);
 	printf("%s\n", key);
 	free(key);
 
-	return 0;
+	return (0);
 }
