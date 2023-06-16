58d100abf4e2106b071a5b2d4259230f761ed9f0
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-06-16 01:18:21 +0100

    fix

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -18,24 +18,23 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	current = *head;
 	if (index == 0)
 	{
-		*head = (*head)->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
+		*head = current->next;
+		if (current->next != NULL)
+		{
+			current->next->prev = NULL;
+		}
 		free(current);
 		return (1);
 	}
-	while (current != NULL)
+	for (i = 0; i < index; i++)
 	{
-		if (i == index)
-		{
-			current->prev->next = current->next;
-			if (current->next != NULL)
-				current->next->prev = current->prev;
-			free(current);
-			return (1);
-		}
-		i++;
+		if (current->next == NULL)
+			return (-1);
 		current = current->next;
 	}
-	return (-1);
+	current->prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	free(current);
+	return (1);
 }
