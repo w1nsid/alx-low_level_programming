332213c845b9aa30484b15c64265b4b707bffa6a
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-06-20 01:23:01 +0100

    .

--- 0x18-dynamic_libraries/cheat.c
+++ 0x18-dynamic_libraries/cheat.c
@@ -1,15 +1,19 @@
 int rand()
 {
-	static int sequence[] = {8, 8, 7, 9, 23, 74};
-	static int sequenceLength = 5;
-	static int currentIndex = 0;
-	int nextElement;
+	static int counter = 0;
 
-	if (currentIndex < sequenceLength)
-		nextElement = sequence[currentIndex];
-	else
-		nextElement = currentIndex * currentIndex % 30000;
-	currentIndex++;
-
-	return nextElement;
+	counter++;
+	if (counter == 1)
+		return 8;
+	if (counter == 2)
+		return 8;
+	if (counter == 3)
+		return 7;
+	if (counter == 4)
+		return 9;
+	if (counter == 5)
+		return 23;
+	if (counter == 6)
+		return 74;
+	return counter * counter % 30000;
 }
